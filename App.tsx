8df112c551340b9f9962a4c2bfbe7834905e1c11
Quyen Hoang <qduhoang@gmail.com>
2023-09-12 18:45:59 -0700

    Title and Divided complete

--- App.tsx
+++ App.tsx
@@ -3,8 +3,11 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 bg-white">
+      <View className="mt-20 px-8 mb-2">
+        <Text className="font-bold text-xl">Markets</Text>
+      </View>
+      <View className="border-[#d3d3d3] border-b-2 w-[90%] m-4" />
       <StatusBar style="auto" />
     </View>
   );
