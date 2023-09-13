d0cb23b293264ab247f26f3473f3cdd94b51ee8f
Quyen Hoang <qduhoang@gmail.com>
2023-09-13 13:17:51 -0700

    Finished BottomSheet Modal and fixed version mismatches

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -1,11 +1,4 @@
-import { View, Text, Image, Dimensions } from 'react-native';
-import {
-  ChartDot,
-  ChartPath,
-  ChartPathProvider,
-} from '@rainbow-me/animated-charts';
-
-export const { width: SIZE } = Dimensions.get('window');
+import { View, Text, Image } from 'react-native';
 
 export default function Chart({
   currentPrice,
@@ -16,43 +9,37 @@ export default function Chart({
   sparkline,
 }: Props) {
   return (
-    <ChartPathProvider
-      data={{ points: sparkline.price, smoothingStrategy: 'bezier' }}
-    >
-      <View className="m-8">
-        {/* Titles */}
-        <View>
-          <View className="flex-row justify-between items-center">
-            <View className="flex-row items-center">
-              <Image
-                className="w-[24px] h-[24px] mr-2"
-                source={{ uri: logoUrl }}
-              />
-              <Text className="text-[#7d7d7d] text-sm">
-                {name} ({symbol.toUpperCase()})
-              </Text>
-            </View>
-            <Text className="">7d</Text>
-          </View>
-          <View className="flex-row justify-between items-center">
-            <Text className="text-lg text-black font-bold">
-              ${currentPrice.toLocaleString('en-US', { currency: 'USD' })}
-            </Text>
-            <Text
-              className={
-                priceChangePercentage7d > 0
-                  ? 'text-md text-green-600'
-                  : 'text-md text-red-600'
-              }
-            >
-              {priceChangePercentage7d.toFixed(2)}%
+    <View className="m-8">
+      {/* Titles */}
+      <View>
+        <View className="flex-row justify-between items-center">
+          <View className="flex-row items-center">
+            <Image
+              className="w-[24px] h-[24px] mr-2"
+              source={{ uri: logoUrl }}
+            />
+            <Text className="text-[#7d7d7d] text-sm">
+              {name} ({symbol.toUpperCase()})
             </Text>
           </View>
+          <Text className="">7d</Text>
         </View>
-        <ChartPath height={SIZE / 2} stroke="black" width={SIZE} />
-        <ChartDot style={{ backgroundColor: 'blue' }} />
+        <View className="flex-row justify-between items-center">
+          <Text className="text-lg text-black font-bold">
+            ${currentPrice.toLocaleString('en-US', { currency: 'USD' })}
+          </Text>
+          <Text
+            className={
+              priceChangePercentage7d > 0
+                ? 'text-md text-green-600'
+                : 'text-md text-red-600'
+            }
+          >
+            {priceChangePercentage7d.toFixed(2)}%
+          </Text>
+        </View>
       </View>
-    </ChartPathProvider>
+    </View>
   );
 }
 
