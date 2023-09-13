d725df028208449513b417055a325c628e3d9871
Quyen Hoang <qduhoang@gmail.com>
2023-09-12 20:15:16 -0700

    1 ListItem complete

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
+import ListItem from './components/ListItem';
+
+import { SAMPLE_DATA } from './assets/data/sampleData';
+
 export default function App() {
   return (
     <View className="flex-1 bg-white">
@@ -8,6 +12,15 @@ export default function App() {
         <Text className="font-bold text-xl">Markets</Text>
       </View>
       <View className="border-[#d3d3d3] border-b-2 w-[90%] m-4" />
+      <ListItem
+        name={SAMPLE_DATA[0].name}
+        symbol={SAMPLE_DATA[0].symbol}
+        currentPrice={SAMPLE_DATA[0].current_price}
+        priceChangePercentage7d={
+          SAMPLE_DATA[0].price_change_percentage_7d_in_currency
+        }
+        logoUrl={SAMPLE_DATA[0].image}
+      />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/ListItem.tsx
@@ -0,0 +1,54 @@
+interface Props {
+  name: string;
+  symbol: string;
+  currentPrice: number;
+  priceChangePercentage7d: number;
+  logoUrl: string;
+}
+
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+
+export default function ListItem({
+  name,
+  symbol,
+  currentPrice,
+  priceChangePercentage7d,
+  logoUrl,
+}: Props) {
+  return (
+    <TouchableOpacity>
+      <View className="flex flex-row px-4 mb-8 items-center justify-between w-[98%]">
+        {/* Left side */}
+        <View className="flex-row items-center">
+          <Image
+            className="h-[48px] w-[48px]"
+            source={{
+              uri: logoUrl,
+            }}
+          />
+          <View className="ml-2">
+            <Text className="text-lg">{name}</Text>
+            <Text className="text-sm text-[#7f7f7f]">
+              {symbol.toUpperCase()}
+            </Text>
+          </View>
+        </View>
+        {/* Right side */}
+        <View className="items-end">
+          <Text className="text-lg">
+            ${currentPrice.toLocaleString('en-US', { currency: 'USD' })}
+          </Text>
+          <Text
+            className={
+              priceChangePercentage7d > 0
+                ? 'text-sm text-green-600'
+                : 'text-sm text-red-600'
+            }
+          >
+            {priceChangePercentage7d.toFixed(2)}%
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+}
